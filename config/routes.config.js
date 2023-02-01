f0d62d4ecb02aca9c78f06ae996e9483cb6b5e70
Ander Zulaika <anderzu85@gmail.com>
2023-02-01 19:42:18 +0100

    iteration 2

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
+
+// import drones controller
 const drones = require('../controllers/drones.controller');
+// import common, home  controller
 const common = require('../controllers/common.controller'); 
 
+//link GET '/' with common controller home
 router.get('/', common.home);
 router.get('/drones', drones.list);
 
 module.exports = router;
+
+
